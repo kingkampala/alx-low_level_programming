3e05a7020cc8fb4cb7608890d6093f0b633804e6
kampala <ezeanyathankgod@gmail.com>
2022-06-03 16:17:50 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - print if the number is positive, zero or negative
  *
- * Return: Always 0 (Success)
+ * Description: using the main function
+ * this program prints "Programming is positive, zero or negative"
+ * Return: 0
  */
 int main(void)
 {
