73483b57c5274f55697c88070a10dd3bdc658d7f
kampala <ezeanyathankgod@gmail.com>
2022-06-07 07:37:42 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Description: write a program that prints "_putchar" followed by a new line
- * Return: Always 0 (Success)
+ * main - prints "_putchar", followed by a new line
+ *
+ * Return: Always (0)
  */
 int main(void)
 {
