8d45152dec7f0fa8f0bd7f8e00c5a7e4847a3ab3
kampala <ezeanyathankgod@gmail.com>
2022-06-07 09:25:01 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints 10 times the alphabet, in lowercase, followed by a new line
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase, followed by a new line
  *
  * Return: Always 0
  */
