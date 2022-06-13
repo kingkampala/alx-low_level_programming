6df26eab78cd60c7f7205602ef5f46a64592b918
kampala <ezeanyathankgod@gmail.com>
2022-06-13 04:03:38 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @str: string
+ *
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	while (*str)
+		_putchar(*str--);
+
+	_putchar('\n');
+}
