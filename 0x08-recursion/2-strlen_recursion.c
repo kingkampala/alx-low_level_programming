62b464d26a919ddf0c4efbbb81205e5d0a4e90ad
kampala <ezeanyathankgod@gmail.com>
2022-06-22 05:22:23 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int l;
+
+	l = strlen(s);
+
+	return (l);
+}
