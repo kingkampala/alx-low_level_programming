1645aa5fdb28371cbed72f91518d323f01c9dc74
kampala <ezeanyathankgod@gmail.com>
2022-06-22 03:47:08 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
