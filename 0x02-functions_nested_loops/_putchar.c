10cdd4b18aceac567d56659c556d5ab34b785e5b
Kampala <104834237+kingkampala@users.noreply.github.com>
2022-06-24 20:59:21 +0100

    Update _putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -5,7 +5,7 @@
  * @c: The charater to print
  *
  * Return: On success 1
- * On erro, -1 is returned, and errno is set appropriately
+ * On error, -1 is returned, and error is set appropriately
  */
 int _putchar(char c)
 {
