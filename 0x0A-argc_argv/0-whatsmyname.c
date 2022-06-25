8339ce31d8f226e687267a90a3e51298fa1606b9
kampala <ezeanyathankgod@gmail.com>
2022-06-24 17:33:55 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", *argv);
+	return (0);
+}
