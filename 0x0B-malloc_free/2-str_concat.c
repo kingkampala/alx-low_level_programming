b073e6cf800f37569a935047f47a946cc7951f2b
kampala <ezeanyathankgod@gmail.com>
2022-06-27 07:17:51 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,7 +10,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
-	int s1, s2;
+	int size1, size2;
 	int x, y;
 
 	if (!s1)
@@ -25,10 +25,10 @@ char *str_concat(char *s1, char *s2)
 	if (!ptr)
 		return (NULL);
 
-	for (x = 0; x < s1; x++)
+	for (x = 0; x < size1; x++)
 		ptr[x] = s1[x];
 
-	for (y = 0; y < (s2 + 1); y++)
+	for (y = 0; y < (size2 + 1); y++)
 		ptr[x + y] = s2[y];
 
 	return (ptr);
