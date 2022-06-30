36fd0906c9ded823f5c4ae86083d40eb2b815e32
kampala <ezeanyathankgod@gmail.com>
2022-06-30 05:24:27 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -40,7 +40,7 @@ void _is_zero(char *argv[])
 }
 
 /**
- * _initialize_array - set memery to zero in a new array
+ * _initialize_array - set memory to zero in a new array
  * @ar: char array
  * @lar: length of the char array
  *
@@ -95,7 +95,7 @@ int main(int argc, char *argv[])
 		printf("Error\n"), exit(98);
 	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
 	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
-	if (!nout)
+	if (nout == NULL)
 		printf("Error\n"), exit(98);
 	nout = _initialize_array(nout, lnout);
 	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
