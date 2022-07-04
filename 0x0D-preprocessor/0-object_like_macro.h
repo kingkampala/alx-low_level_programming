c5a77b82e2ad7c5c567aa1d91912d7b2be3a68ad
kampala <ezeanyathankgod@gmail.com>
2022-07-04 09:09:11 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stlib.h>
+
+#define SIZE 1024
+
+/**
+ * main - Entry point
+ * defines a macro named SIZE
+ * as an abbreviation for the token 1024
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x = SIZE;
+	return (0);
+}
