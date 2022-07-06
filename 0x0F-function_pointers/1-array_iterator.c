62e7f5591c9b71fc84112b542d3cfa3674372cd4
kampala <ezeanyathankgod@gmail.com>
2022-07-06 15:09:09 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (!array || !action)
 		return;
-	
-		for (x = 0; x <= size; x++)
-			action(array[x]);
+	for (x = 0; x < size; x++)
+		action(array[x]);
 }
