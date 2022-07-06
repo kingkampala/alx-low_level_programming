385b1f53be17f0e542357e1ea1ddd3f3429bcd94
kampala <ezeanyathankgod@gmail.com>
2022-07-06 14:40:19 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer function
+ * Return: Always 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		f(name);
+}
