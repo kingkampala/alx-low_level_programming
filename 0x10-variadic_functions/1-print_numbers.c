498347316e51bb56a8744f8319c7d23aee2ab8a9
kampala <ezeanyathankgod@gmail.com>
2022-07-07 06:46:00 -0700

    1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print numbers
+ * followed by a new line
+ * @separator: string to be printed
+ * @n: number of integers passed
+ * Return: Always 0
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list kampala;
+	unsigned int x;
+
+	va_start(kampala, n);
+	for (x = 0; x < n; x++)
+	{
+		printf("%d", va_arg(kampala, int));
+		if (x < n - 1 && !separator)
+			printf("%s", separator);
+	}
+	va_end(kampala);
+	printf("\n");
+}
