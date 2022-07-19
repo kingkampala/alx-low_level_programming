88f8541300d93abf56226ffc166ae508358f28a2
kampala <ezeanyathankgod@gmail.com>
2022-07-19 12:12:19 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -3,7 +3,8 @@
 /**
  * delete_nodeint_at_index - deletes the node at index
  * (index) of a listint_t linked list
- * @index: index of the node that should be deleted. Index starts at 0
+ * @index: index of the node that should be deleted
+ * index starts at 0
  * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
