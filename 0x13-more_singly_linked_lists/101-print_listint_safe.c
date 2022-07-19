450bb750c183f9e0c1bac6d27af33184c14d1c4e
kampala <ezeanyathankgod@gmail.com>
2022-07-19 14:22:27 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,6 +3,7 @@
 /**
  * print_listint_safe - prints a listint_t linked list
  * @head: pointer to head of list
+ *
  * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
