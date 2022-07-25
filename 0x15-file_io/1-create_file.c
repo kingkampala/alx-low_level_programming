241df3f3164865fb8d365e3739538f4133c7e627
kampala <ezeanyathankgod@gmail.com>
2022-07-25 08:08:15 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, t, s = 0;
 
-	(!filename)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
