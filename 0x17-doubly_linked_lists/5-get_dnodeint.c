171d3ca303a6d689c57672a64076cdca38a9e6a0
kampala <ezeanyathankgod@gmail.com>
2022-09-01 13:01:37 -0700

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * get_dnodeint - returns the nth node of a dlistint_t linked list
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
  * @head: pointer to head node
  * @index: index of the nth node
  * Return: nth node or null
