78ee6f3233a888d205b923eb7e2575f2d4b4e688
kampala <ezeanyathankgod@gmail.com>
2022-09-05 09:41:22 -0700

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp ../sh2.sh .
-. ./sh2.sh
+wget -P /tmp/ https://raw.github.com/kingkampala/alx-low_level_programming/master/0x18-dynamic_libraries/preload.so
+export LD_PRELOAD=/tmp/preload.so
